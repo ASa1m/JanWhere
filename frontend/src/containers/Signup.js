bf5c5842bad6fc39a776515f251355e2c8a841fb
Abdullah Saim <asaim.bscs21seecs@seecs.edu.pk>
2023-05-09 00:55:19 +0500

    Login Features Underway

--- frontend/src/containers/Signup.js
+++ frontend/src/containers/Signup.js
@@ -6,6 +6,26 @@ import Button from "react-bootstrap/Button";
 
 import "../styles/App.css";
 
+async function loginUser(credentials) {
+
+    return fetch('http://localhost:4000/signup', {
+   
+      method: 'POST',
+   
+      headers: {
+   
+        'Content-Type': 'application/json'
+   
+      },
+   
+      body: JSON.stringify(credentials)
+   
+    })
+   
+      .then(data => data.json())
+   
+   }
+
 export default function Login() {
 
     const [email, setEmail] = useState("");
