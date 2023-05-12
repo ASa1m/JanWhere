d99a2e32611746ea7781898bf46c7c41807665e5
Mehran Sangrasi <mehransangrasi@gmail.com>
2023-05-13 02:58:13 +0500

    Added Post Cards

--- frontend/src/components/layout/DiscoverCard.js
+++ frontend/src/components/layout/DiscoverCard.js
@@ -1,35 +1,97 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import React from 'react';
-import PlaceHoder from '../../placeholder-image.png';
 import { useNavigate } from 'react-router-dom';
+import PlaceHoder from '../../placeholder-image.png';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-function RCard(props) {
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
+export default function DiscoverCard(props) {
+  const [expanded, setExpanded] = React.useState(false);
   const navigate = useNavigate();
 
-  const handleClick = () => {
-      console.log(props.id);
-      navigate('/post/' + props.id);
-  }
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+const handleClick = () => { 
+  navigate('/post/'+props.id);
+}
+
 
   return (
-    <Card className={"card flex-sm-column flex-lg-"+ ((props.direction === 'right') ? 'row' : 'row-reverse')} >
-        <div className={"col-lg-4 col-md-12  h-100"}>
-        <Card.Img className='img-fluid bg-img' variant="top" src={props.image === '' ? PlaceHoder : props.image} alt="Animal Placeholder" width ={'100%'} height={'auto'} />
-        </div>
-        <div className= "card-content col-8 h-100">
-      <Card.Header as="h5">{props.feature}</Card.Header>
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text className="text-truncate" style={{ margin: '1rem 0' }}>
-            {props.description}
-        </Card.Text>
-        <Button variant="primary" style={{backgroundColor: '#FFC947', color: '#46237A'}} onClick={handleClick}>Learn More</Button>
-      </Card.Body>
-        </div>
+    <Card className='m-2' sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {props.username?props.username[0]:"U"}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.username ? props.username : "Unknown"}           //props.Username
+        subheader={props.date ? props.date : "1-Jan-2023"   } //props.Date
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={props.image ? props.image : PlaceHoder }  //props.Image
+        alt={props.title ? props.title+" image" : "Image not found"  } //props.Title
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {props.cover ? props.cover : "No cover"  }   
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            {props.Content ? props.Content : "No content" } // props.Content
+          </Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
-
-export default RCard;
--- frontend/src/containers/Discover.js
+++ frontend/src/containers/Discover.js
@@ -25,16 +25,18 @@ export default class Dicover extends Component {
 
     fetchData() {
         return this.state.animals.map((res, i) => {
-            return <Card direction= {i % 2 === 0 ? 'left' : 'right'} feature={res.category} title={res.name} description={res.description} image={res.image} id={res._id} />;
+            return <Card obj={res} key={i} />; 
         });
     }
 
   render() {
     return (<div className="content" style={{ fontFamily: "monospace" }}>
       <h3 className="center text-white">Discover the world of animals</h3>
-       <div>
+       <div className="d-flex flex-wrap justify-content-center">
         {this.fetchData()}
         </div>
     </div>);
   }
 }
+
+
