b25d8cfec8ac913acf884bd17ef111b0a174bf27
Mehran Sangrasi <mehransangrasi@gmail.com>
2023-05-16 17:04:28 +0500

    added fromspree

--- frontend/src/containers/Contact.js
+++ frontend/src/containers/Contact.js
@@ -54,7 +54,7 @@ const Contact = () => {
       <div className="container">
         <div className="contact-form">
           <form
-            action="https://formspree.io/f/xgedgjkr"
+            action="https://formspree.io/f/xwkjzwen"
             method="POST"
             className="contact-inputs">
              <Box
